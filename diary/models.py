805b911bba503effab9d8a023f51f241f6f57ae8
NamCheolKim <pkeowkd@naver.com>
2021-05-13 02:30:01 +0900

    UI 수정중

--- diary/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# 기록하기, 상세내용
-class PostDiary(models.Model):
-    title = models.CharField(max_length=30)
-    content = models.TextField()
-    image = models.ImageField(upload_to="upload/%Y/%m/%d/", blank=True)
-    created_at = models.DateTimeField()
